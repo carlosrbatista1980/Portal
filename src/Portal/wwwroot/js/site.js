e4aa06c2f8d33bec6fbb2101730a79a599025970
Carlos Rodrigues Batista <carlosrbatista1980@gmail.com>
2020-02-04 18:12:22 -0300

    first commit

---
+++ src/Portal/wwwroot/js/site.js
@@ -0,0 +1,32 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+(function() {
+    'use strict';
+
+    $(onJQueryLoad);
+    function onJQueryLoad() {
+        var $loginForm = $('#LoginForm');
+        $("#LoginButton").click(onLoginButtonClick);
+
+        function onLoginButtonClick(e) {
+            e.preventDefault();
+
+            var dataToParse = {
+                    tenancyName: $("#TenancyName").val(),
+                    usernameOrEmailAddress: $("#EmailAddressInput").val(),
+                    password: $("#PasswordInput").val(),
+                    rememberMe: $("#RememberMeInput").is(":checked")
+                },
+                ajaxConfiguration = {
+                    url: $loginForm.attr('action'),
+                    type: "POST",
+                    data: JSON.stringify(dataToParse),
+                };
+
+            abp.ui.setBusy($("#LoginArea"), abp.ajax(ajaxConfiguration));
+        }
+    }
+}());
